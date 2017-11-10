15b10445200d36d311a952f5250c50f7dbb0e63c
odlomax <o_d_lomax@live.co.uk>
2017-11-10 19:28:49 +0000

    gaussian random field can have any number of dimensions

--- maths/fields/gaussian_random_field.py
+++ maths/fields/gaussian_random_field.py
@@ -13,17 +13,18 @@ class scalar_grf:
         signal of gaussian random field
     
     """
-    
+        
+        
     def __init__(self,n_shape,alpha,real_field):
         
         """
-        Subroutine: wrapper for dimensional init routines
+        Subroutine: generate a nd random gaussian field with a power spectrum
         
         Arguments
         ---------
         
-        n_shape(,) int tuple
-            list of dimensions
+        n_shape(,): tuple, int
+            shape of field
             
         alpha: float
             power spectrum exponent
@@ -33,114 +34,23 @@ class scalar_grf:
         
         """
         
-        
-        if len(n_shape)==1:
-            self.init_1d(n_shape[0],alpha,real_field)
-        elif len(n_shape)==2:
-            self.init_2d(n_shape[0],n_shape[1],alpha,real_field)
-        elif len(n_shape)==3:
-            self.init_3d(n_shape[0],n_shape[1],n_shape[2],alpha,real_field)
-        else:
-            print("Cannot make field with rank",len(n_shape))
-            
-            
-    def init_1d(self,n_x,alpha,real_field):
-        
-        """
-        Subroutine: generate a 1D random gaussian field with a power spectrum
-        
-        Arguments
-        ---------
-        
-        n_dim: int
-            number of dimensions
-            
-        n_x: int
-            edge-length of field
-            
-        alpha: float
-            power spectrum exponent
-        
-        real_field: boolean
-            is field real?
-        
-        """
-        
-        # create array of k values from [-n_x/2:n_x/2]
-        if np.mod(n_x,2)==0:
-            k=np.arange(-n_x//2,n_x//2+1,dtype=np.float)
-        else:
-            k=np.arange(-n_x//2+1,n_x//2+1,dtype=np.float)
-            
-        # create amplitude array a(k)
-        a_k=np.where(np.abs(k)>0,np.abs(k)**(-0.5*alpha),0.)
-        
-        # create phase array phi(k)
-        phi_k=np.random.random(k.shape)*2.*np.pi
-        
-        # make spetrum Hermitian if signal is real
-        if real_field:
-            # make array of phi(-k)
-            phi_k_minus=phi_k[::-1]            
-            phi_k=phi_k-phi_k_minus
-        
-        # create complex spectrum
-        self.spectrum=np.zeros(k.shape,dtype=np.complex)
-        self.spectrum.real=a_k*np.cos(phi_k)
-        self.spectrum.imag=a_k*np.sin(phi_k)
-        
-        # resize and shift array (if size is even, n//2 frequency is superposition of n//2 and -n//2)
-        if np.mod(n_x,2)==0:
-            self.spectrum[0]+=self.spectrum[-1]
-            self.spectrum=self.spectrum[:n_x]
-            self.spectrum=np.roll(self.spectrum,n_x//2)
-        else:
-            self.spectrum=np.roll(self.spectrum,n_x//2+1)
-        
-        # perform FFT
-        self.signal=np.fft.ifft(self.spectrum)
-        
-        
-    def init_2d(self,n_x,n_y,alpha,real_field):
-        
-        """
-        Subroutine: generate a 2D random gaussian field with a power spectrum
-        
-        Arguments
-        ---------
-        
-        n_dim: int
-            number of dimensions
-            
-        n_x,n_y: int
-            edge-length of field
-            
-        alpha: float
-            power spectrum exponent
-        
-        real_field: boolean
-            is field real?
-        
-        """
-        
-        # create array of k_x values from [-n_x/2:n_x/2]
-        if np.mod(n_x,2)==0:
-            k_x=np.arange(-n_x//2,n_x//2+1,dtype=np.float)
-        else:
-            k_x=np.arange(-n_x//2+1,n_x//2+1,dtype=np.float)
-            
-        # create array of k_y values from [-n_y/2:n_y/2]
-        if np.mod(n_y,2)==0:
-            k_y=np.arange(-n_y//2,n_y//2+1,dtype=np.float)
-        else:
-            k_y=np.arange(-n_y//2+1,n_y//2+1,dtype=np.float)
-            
-        # convert k_x and k_y to grid format
-        k_x,k_y=np.meshgrid(k_x,k_y,indexing='ij')
-        k=np.sqrt(k_x**2+k_y**2)
+        # make k coordinate arrays
+        k_list=[]
+        for i in n_shape:
+            k_list.append(np.arange(-i//2+np.mod(i,2),i//2+1))
 
+        # generate array of k values
+        if len(n_shape)>1:
+            k=np.zeros(np.array(n_shape)+np.mod(np.array(n_shape)+1,2))
+            for i in np.meshgrid(*k_list,indexing="ij"):
+                k+=i**2
+        else:
+            k=k_list[0]**2
+        k=np.sqrt(k)
+            
         # create amplitude array a(k)
-        a_k=np.where(k>0,k**(-0.5*(alpha+1.)),0.)
+        a_k=np.where(k>0,k**(-0.5*(alpha+len(n_shape)-1)),0.)
+        a_k/=np.sqrt((a_k**2).sum())
         
         # create phase array phi(k)
         phi_k=np.random.random(k.shape)*2.*np.pi
@@ -148,7 +58,7 @@ class scalar_grf:
         # make spetrum Hermitian if signal is real
         if real_field:
             # make array of phi(-k)
-            phi_k_minus=phi_k[::-1,::-1]            
+            phi_k_minus=phi_k[[slice(None,None,-1)]*len(n_shape)]
             phi_k=phi_k-phi_k_minus
         
         # create complex spectrum
@@ -156,113 +66,26 @@ class scalar_grf:
         self.spectrum.real=a_k*np.cos(phi_k)
         self.spectrum.imag=a_k*np.sin(phi_k)
         
-        # resize and shift array (if size is even, n//2 frequency is superposition of n//2 and -n//2)
-        if np.mod(n_x,2)==0:
-            
-            self.spectrum[0,:]+=self.spectrum[-1,:]
-            self.spectrum=self.spectrum[:n_x,:]
-            
-            self.spectrum=np.roll(self.spectrum,n_x//2,0)
-        else:
-            self.spectrum=np.roll(self.spectrum,n_x//2+1,0)
-            
-        if np.mod(n_y,2)==0:
-            
-            self.spectrum[:,0]+=self.spectrum[:,-1]
-            self.spectrum=self.spectrum[:,:n_y]
-            
-            self.spectrum=np.roll(self.spectrum,n_y//2,1)
-        else:
-            self.spectrum=np.roll(self.spectrum,n_y//2+1,1)
+        # resize array (if size is even, n//2 frequency is superposition of n//2 and -n//2)
+        for i in range(len(n_shape)):
+            if np.mod(n_shape[i],2)==0:
+                i_plus=[slice(None)]*len(n_shape)
+                i_plus[i]=-1
+                i_minus=[slice(None)]*len(n_shape)
+                i_minus[i]=0
+                self.spectrum[i_minus]+=self.spectrum[i_plus]
+                indices=[slice(None)]*len(n_shape)
+                indices[i]=slice(None,-1)
+                self.spectrum=self.spectrum[indices]
+                
+        # shift array
+        offset=(np.array(n_shape)+np.mod(np.array(n_shape),2))//2
+        axis=np.arange(len(n_shape))
+        self.spectrum=np.roll(self.spectrum,offset,axis)
             
         # perform FFT
-        self.signal=np.fft.ifftn(self.spectrum)
+        self.signal=self.spectrum.size*np.fft.ifftn(self.spectrum)
         
-    def init_3d(self,n_x,n_y,n_z,alpha,real_field):
-        
-        """
-        Subroutine: generate a 3D random gaussian field with a power spectrum
-        
-        Arguments
-        ---------
-        
-        n_dim: int
-            number of dimensions
-            
-        n_x,n_y,n_z: int
-            edge-length of field
-            
-        alpha: float
-            power spectrum exponent
-        
-        real_field: boolean
-            is field real?
-        
-        """
-        
-        # create array of k_x values from [-n_x/2:n_x/2]
-        if np.mod(n_x,2)==0:
-            k_x=np.arange(-n_x//2,n_x//2+1,dtype=np.float)
-        else:
-            k_x=np.arange(-n_x//2+1,n_x//2+1,dtype=np.float)
-            
-        # create array of k_y values from [-n_y/2:n_y/2]
-        if np.mod(n_y,2)==0:
-            k_y=np.arange(-n_y//2,n_y//2+1,dtype=np.float)
-        else:
-            k_y=np.arange(-n_y//2+1,n_y//2+1,dtype=np.float)
-            
-        # create array of k_z values from [-n_z/2:n_z/2]
-        if np.mod(n_z,2)==0:
-            k_z=np.arange(-n_z//2,n_z//2+1,dtype=np.float)
-        else:
-            k_z=np.arange(-n_z//2+1,n_z//2+1,dtype=np.float)
-            
-        # convert k_x and k_y to grid format
-        k_x,k_y,k_z=np.meshgrid(k_x,k_y,k_z,indexing='ij')
-        k=np.sqrt(k_x**2+k_y**2+k_z**2)
-            
-        # create amplitude array a(k)
-        a_k=np.where(k>0,k**(-0.5*(alpha+2.)),0.)
-        
-        # create phase array phi(k)
-        phi_k=np.random.random(k.shape)*2.*np.pi
-        
-        # make spetrum Hermitian if signal is real
-        if real_field:
-            # make array of phi(-k)
-            phi_k_minus=phi_k[::-1,::-1,::-1]
-            phi_k=phi_k-phi_k_minus
-        
-        # create complex spectrum
-        self.spectrum=np.zeros(k.shape,dtype=np.complex)
-        self.spectrum.real=a_k*np.cos(phi_k)
-        self.spectrum.imag=a_k*np.sin(phi_k)
-        
-        # resize and shift array (if size is even, n//2 frequency is superposition of n//2 and -n//2)
-        if np.mod(n_x,2)==0:
-            self.spectrum[0,:,:]+=self.spectrum[-1,:,:]
-            self.spectrum=self.spectrum[:n_x,:,:]
-            self.spectrum=np.roll(self.spectrum,n_x//2,0)
-        else:
-            self.spectrum=np.roll(self.spectrum,n_x//2+1,0)
-        
-        if np.mod(n_y,2)==0:
-            self.spectrum[:,0,:]+=self.spectrum[:,-1,:]
-            self.spectrum=self.spectrum[:,:n_y,:]
-            self.spectrum=np.roll(self.spectrum,n_y//2,1)
-        else:
-            self.spectrum=np.roll(self.spectrum,n_y//2+1,1)
-            
-        if np.mod(n_z,2)==0:
-            self.spectrum[:,:,0]+=self.spectrum[:,:,-1]
-            self.spectrum=self.spectrum[:,:,:n_z]
-            self.spectrum=np.roll(self.spectrum,n_z//2,2)
-        else:
-            self.spectrum=np.roll(self.spectrum,n_z//2+1,2)
-            
-        # perform FFT
-        self.signal=np.fft.ifftn(self.spectrum)
         
     def normalise(self,sigma=1.,exponentiate=False,exp_base=np.e):
         
@@ -289,6 +112,9 @@ class scalar_grf:
         
         if exponentiate:
             self.signal=np.exp(self.signal*np.log(exp_base))
+            
+        # update spectrum
+        self.spectrum=np.fft.fftn(self.signal)
         
 
     def com_shift(self):
@@ -320,3 +146,57 @@ class scalar_grf:
             # shift array
             self.signal=np.roll(self.signal,self.signal.shape[i]//2-shift,i)
             
+        # update spectrum
+        self.spectrum=np.fft.fftn(self.signal)
+        
+    def gauss_conv(self,sigma):
+        
+        """
+        
+        Subroutine: applies Gaussian convolution to signal
+        
+        Arguments
+        ---------
+        
+        sigma: float
+            width of Gaussian (pixels)
+            
+        """ 
+            
+        # make list of axis coodinates
+        axes=[]
+        for i in self.signal.shape:
+            axes.append(np.arange(i)-(i-1)/2.)
+                        
+        # make r squared array
+        if len(self.signal.shape)>1:
+            r_sqd=np.zeros(self.signal.shape)
+            for j in np.meshgrid(*axes,indexing="ij"):
+                r_sqd+=j**2
+        else:
+            r_sqd=axes[0]**2
+        
+        # generate kernel function
+        kernel=np.zeros(self.signal.shape,dtype=np.complex)
+        kernel.real=np.exp(-r_sqd/(2.*sigma**2))
+        
+        # normalise and shift kernel
+        kernel.real/=np.sum(kernel.real)
+        offset=(np.array(kernel.shape)+np.mod(np.array(kernel.shape),2))//2
+        axis=np.arange(len(kernel.shape))
+        kernel=np.roll(kernel,offset,axis)
+        
+        # convolve signal with kernel
+        self.spectrum=np.fft.fftn(self.signal)
+        self.spectrum*=np.fft.fftn(kernel)
+        self.signal=np.fft.ifftn(self.spectrum)
+        
+        
+        
+            
+        
+        
+        
+        
+        
+        
